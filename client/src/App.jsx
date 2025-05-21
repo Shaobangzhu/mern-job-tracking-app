b5635dfd4944ea1dfc6a90871ab911094555909c
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-21 13:14:50 -0700

    create src/pages dir, set-up index.js and a lit of pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,23 @@
-function App() {
-  return <h1>Jobify App</h1>;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomeLayout from './pages/HomeLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />
+  },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h1>About Page</h1>
+      </div>
+    ),
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router}/>;
 }
 
 export default App
