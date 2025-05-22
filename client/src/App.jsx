2e8cb32d717114a95bf3f522d973da353fb5580c
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-21 17:18:40 -0700

    Nested Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,46 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HomeLayout, AddJob, Admin, AllJobs, DashboardLayout, EditJob, Error, Landing, Login, Profile, Register, Stats } from './pages';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  AddJob,
+  Admin,
+  AllJobs,
+  DashboardLayout,
+  EditJob,
+  Error,
+  Landing,
+  Login,
+  Profile,
+  Register,
+  Stats,
+} from "./pages";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <HomeLayout />
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
   },
-  {
-    path: '/register',
-    element: <Register />
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-    path: '/dashboard',
-    element: <DashboardLayout />
-  }
 ]);
 
 const App = () => {
-  return <RouterProvider router={router}/>;
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
