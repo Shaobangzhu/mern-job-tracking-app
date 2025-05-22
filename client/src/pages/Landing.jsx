c196df7548b2ddecf0eb47adeebf2dbfa8551c44
Chaoran Lu <andyluloveusc@gmail.com>
2025-05-21 19:06:14 -0700

    introduction to styled component

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+    font-size: 1.5rem;
+    background: red;
+    color: white;
+`;
+
 const Landing = () => {
-    return <h1>Landing</h1>
+    return (
+        <div>
+            <h1>Landing Page</h1>
+            <StyledBtn>styled btn</StyledBtn>
+        </div>
+    )
 };
 
 export default Landing;
