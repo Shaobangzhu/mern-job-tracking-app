63f708ddaad66f2a22b82ad8075fc2b8c7be25eb
Chaoran Lu <andyluloveusc@gmail.com>
2025-06-24 12:30:47 -0700

    set up validation middleware

--- server.js
+++ server.js
@@ -1,49 +1,54 @@
-import express from 'express';
-import 'express-async-errors';
-import morgan from 'morgan';
-import * as dotenv from 'dotenv';
-import jobRouter from './routes/jobRouter.js';
-import mongoose from 'mongoose';
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import jobRouter from "./routes/jobRouter.js";
+import mongoose from "mongoose";
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { validateTest } from "./middleware/validationMiddleware.js";
 
 dotenv.config();
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.post('/', (req, res) => {
-    console.log(req);
-    res.json({ message: 'data received', data: req.body });
-});
+app.post(
+  "/api/v1/test",
+  validateTest,
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello ${name}` });
+  }
+);
 
-app.use('/api/v1/jobs', jobRouter);
+app.use("/api/v1/jobs", jobRouter);
 
 // Not Found Middleware
 
-app.use('*', (req, res) => {
-    res.status(404).json({ msg: 'not found' });
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
 });
 
 // Error Middleware
 
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5100
+const port = process.env.PORT || 5100;
 
 try {
-    await mongoose.connect(process.env.MONGO_URL);
-    app.listen(port, () => {
-        console.log(`server running on PORT ${port}...`);
-    });
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}...`);
+  });
 } catch (error) {
-    console.log(error);
-    process.exit(1);
+  console.log(error);
+  process.exit(1);
 }
